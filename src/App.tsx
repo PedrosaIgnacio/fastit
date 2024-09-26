ed53c051a1c0550b4873afffff37b8645e010efc
PedrosaIgnacio <ipedrosa.dev@gmail.com>
2024-09-26 18:57:34 -0300

    asd

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
-import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
